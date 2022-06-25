3222570921772f458ce89a77f46d425958e9f973
Anselmo Oliveira <anselmo.oliveira@dataway.info>
2022-06-25 18:51:24 +0000

    Primeiro Commit

---
+++ .orchest/environments/6449305e-5170-4e80-84d9-5e9c3ca98522/setup_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install numpy
+pip install beautifulsoup4
+pip install geopandas
+pip install pandas
+pip install psycopg2-binary
+pip install sqlalchemy
+pip install matplotlib
+pip install seaborn
+pip install pg8000
